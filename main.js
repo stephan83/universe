0a87f00b0f73195f19db558e1ddcf43679c7741a
Stephan Florquin <stephan.florquin@gmail.com>
2015-07-27 02:53:30 +0200

    Pew pew pew

--- main.js
+++ main.js
@@ -1,252 +1,258 @@
 !function() {
 
-  // Possible movements for each side (currently the same)
-  var movements = [
-    [[-1,-1],[-2,0],[-1,1],[0,-2],[0,2],[1,-1],[2,0],[1,1]],
-    [[-1,-1],[-2,0],[-1,1],[0,-2],[0,2],[1,-1],[2,0],[1,1]],
-    [[-1,-1],[-2,0],[-1,1],[0,-2],[0,2],[1,-1],[2,0],[1,1]]
+  var DIRECTIONS = [
+    [-1, 0],
+    [0, 1],
+    [1, -1],
+    [1, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 1],
+    [-1, -1]
   ];
 
-  // Frame used by the players
-  var frame = new Frame();
+  var wallsFrame = new Frame();
+  var playersFrame = new Frame();
+  var wavesFrame = new Frame();
+  var missilesFrame = new Frame();
 
-  // Frame used by the energy source sources
-  var energyFrame = new Frame();
-
-  var zoom = 1;
+  var zoom = 2;
   var viewX = 0;
   var viewY = 0;
-
-  var recycledEnergy = 0;
-  var cycle = 0;
-
-  var stats = [
-    {count: 0}, {count: 0}, {count: 0}
-  ];
 
   // Set up canvas and context
   var canvas = document.getElementById('universe');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   var ctx = canvas.getContext('2d');
+  ctx.imageSmoothingEnabled = false;
 
-  // Add random players to the frame
-  for (var i = 0; i < 1000; i++) {
-    var side = Math.floor(Math.random() * 3);
-
-    do {
-      var startX = Math.floor(Math.random() * 200) - 100;
-      var startY = Math.floor(Math.random() * 100) - 50;
-    } while(frame.read(startX, startY));
-
-    var pattern = [];
-
-    for (var j = 0; j < Math.ceil(Math.random() * 200); j++) {
-      pattern.push(Math.floor(Math.random() * movements[side].length));
-    }
-
-    var data = {
-      amp: 500,
-      pattern: pattern,
-      step: 0,
-      side: side,
-      startX: startX,
-      startY: startY,
-      cloneThreshold: Math.floor(Math.random() * 1000),
-      inverse: false
-    };
-
-    frame.write(startX, startY, data);
-
-    stats[side].count++;
+  // Used to colorize the walls
+  function wallsColorizer(player) {
+    return 'rgb(200,120,100)';
   }
 
-  // Add random energy sources
-  for (var i = 0; i < 100; i++) {
-    energyFrame.write(
-      Math.floor(Math.random() * 300) - 150,
-      Math.floor(Math.random() * 200) - 100,
+  // Used to colorize the player's squares
+  function playersColorizer(player) {
+    return 'rgb(255,255,255)';
+  }
+
+  // Used to colorize the waves squares
+  function wavesColorizer(wave) {
+    return 'rgb(0,60,60)';
+  }
+
+  // Used to colorize the missiles squares
+  function missilesColorizer(missile) {
+    return 'rgb(255,200,0)';
+  }
+
+  // Renders the frame
+  function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    Renderer.render(ctx, wallsFrame, viewX, viewY, zoom, wallsColorizer);
+    Renderer.render(ctx, wavesFrame, viewX, viewY, zoom, wavesColorizer);
+    Renderer.render(ctx, playersFrame, viewX, viewY, zoom, playersColorizer);
+    Renderer.render(ctx, missilesFrame, viewX, viewY, zoom, missilesColorizer);
+  }
+
+  for (var i = 0; i < 200; i++) {
+    playersFrame.write(
+      Math.floor(Math.random() * 180) - 90,
+      Math.floor(Math.random() * 180) - 90,
       {
-        amp: Math.floor(Math.random() * 100000)
+        name: 'Player ' + (i + 1),
+        health: 100,
+        ammo: 10,
+        waveSensors: {
+          waves: [0,0,0,0,0,0,0,0],
+          missileSensors: [0,0,0,0,0,0,0,0]
+        }
       }
     );
   }
 
-  // Clones a player
-  function clone(frame, data, x, y) {
-    var pattern = data.pattern.slice();
-
-    for (var i = 0; i < 10; i++) {
-      var index = Math.floor(Math.random() * pattern.length);
-      pattern[index] = Math.floor(Math.random() *  movements[data.side].length);
-    }
-
-    var data = {
-      amp: data.cloneThreshold,
-      pattern: pattern,
-      step: Math.max(0, data.step - 1),
-      side: data.side,
-      startX: x,
-      startY: y,
-      cloneThreshold: Math.max(1, data.cloneThreshold - 20 + Math.floor(Math.random() * 20)),
-      inverse: Math.random() < 0.1 ? data.inverse : !data.inverse
-    };
-
-    frame.write(x, y, data);
-
-    return data;
+  for (i = -100; i <= 100; i++) {
+    wallsFrame.write(i, 100, true);
+    wallsFrame.write(i, -100, true);
+    wallsFrame.write(100, i, true);
+    wallsFrame.write(-100, i, true);
   }
 
-  // Used to colorize the player's squares
-  function colorizer(data) {
-    var intensity = Math.min(255, 155 + 200 * (data.amp / 100));
-    if (data.side === 2) {
-      return 'rgb(' + intensity + ',0,' + intensity + ')';
+  function emitWave(player, x, y) {
+    var initialWaveEnergy = 100;
+    for (var i = 0; i < DIRECTIONS.length; i++) {
+      var direction = DIRECTIONS[i];
+      var startX = x + direction[0];
+      var startY = y + direction[1];
+
+      if (wallsFrame.read(startX, startY)) {
+        return;
+      }
+
+      wavesFrame.write(startX, startY, {
+        emitter: player.name,
+        direction: i,
+        energy: initialWaveEnergy
+      });
     }
-    if (data.side === 1) {
-      return 'rgb(0,' + intensity + ',' + intensity + ')';
-    }
-    return 'rgb(' + intensity + ',' + intensity + ',0)';
   }
 
-  // Renders the frames
-  function render() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  function movePlayer(player, x, y, dir) {
+    var direction = DIRECTIONS[dir];
+    var newX = x + direction[0];
+    var newY = y + direction[1];
 
-    Renderer.render(ctx, frame, viewX, viewY, zoom, colorizer);
-    EnergyRenderer.render(ctx, energyFrame, viewX, viewY, zoom);
+    if (wallsFrame.read(newX, newY)) {
+      return;
+    }
+
+    var existing = playersFrame.read(newY, newY);
+
+    if (!existing) {
+      playersFrame.remove(x, y);
+      playersFrame.write(newX, newY, player);
+      emitWave(player, newX, newY);
+    }
+  }
+
+  function fireMissile(player, x, y, dir) {
+    var initialMissilesEnergy = 100;
+
+    var direction = DIRECTIONS[dir];
+    var startX = x + direction[0];
+    var startY = y + direction[1];
+
+    if (wallsFrame.read(startX, startY)) {
+      return;
+    }
+
+    player.ammo -= 5;
+    playersFrame.write(x, y, player);
+
+    var existing = missilesFrame.read(startY, startY);
+
+    if (existing) {
+      missilesFrame.remove(startX, startY);
+    } else {
+      missilesFrame.write(startX, startY, {
+        direction: dir,
+        energy: initialMissilesEnergy
+      });
+      emitWave(player, startX, startY);
+    }
   }
 
   // Main game loop
   function mainLoop() {
-    cycle++;
-
-    // Add lost energy every now and then
-    if (recycledEnergy && Math.random() < 0.05) {
-      energyFrame.write(
-        Math.floor(Math.random() * 300) - 150,
-        Math.floor(Math.random() * 200) - 100,
-        {
-          amp: recycledEnergy
-        }
-      );
-      recycledEnergy = 0;
-    }
-
     render();
 
-    document.getElementById('stats').innerText = 'C: ' + cycle;
-    document.getElementById('stats').innerText += '; T: ' + frame.getTotal();
-    document.getElementById('stats').innerText += '; Y: ' + stats[0].count;
-    document.getElementById('stats').innerText += '; B: ' + stats[1].count;
-    document.getElementById('stats').innerText += '; P: ' + stats[2].count;
+    for (var i = 0; i < 3; i++) {
+      wavesFrame.each(function(x, y, wave) {
+        wavesFrame.remove(x, y);
 
-    if (frame.getTotal() < 1) {
-      return;
+        wave.energy--;
+
+        if (wave.energy > 0) {
+          var direction = DIRECTIONS[wave.direction];
+          var newX = x + direction[0];
+          var newY = y + direction[1];
+
+          if (wallsFrame.read(newX, newY)) {
+            return;
+          }
+
+          var existing = wavesFrame.read(newY, newY);
+
+          if (!existing || wave.energy >= existing.energy) {
+            var player = playersFrame.read(newX, newY);
+
+            if (player && player.name != wave.emitter) {
+              player.waveSensors[wave.direction] += wave.energy;
+              playersFrame.write(newX, newY, player);
+            } else {
+              wavesFrame.write(newX, newY, wave);
+            }
+          }
+        }
+      });
     }
 
-    // Purple team plays twice
-    frame.each(function(x, y, data) {
-      if (data.side === 2) {
-        innerLoop(x, y, data, true);
-      }
-    });
+    for (i = 0; i < 2; i++) {
+      missilesFrame.each(function(x, y, missile) {
+        missilesFrame.remove(x, y);
 
-    frame.each(function(x, y, data) {
-      innerLoop(x, y, data);
+        missile.energy--;
+
+        if (missile.energy > 0) {
+          var direction = DIRECTIONS[missile.direction];
+          var newX = x + direction[0];
+          var newY = y + direction[1];
+
+          if (wallsFrame.read(newX, newY)) {
+            return;
+          }
+
+          var existing = missilesFrame.read(newY, newY);
+
+          if (!existing || missile.energy >= existing.energy) {
+            var player = playersFrame.read(newX, newY);
+
+            if (player) {
+              player.health -= missile.energy;
+
+              if (player.health < 0) {
+                playersFrame.remove(newX, newY);
+              } else {
+                player.missileSensors[missile.direction] += missile.energy;
+                playersFrame.write(newX, newY, player);
+              }
+            } else {
+              missilesFrame.write(newX, newY, missile);
+            }
+          }
+        }
+      });
+    }
+    
+    playersFrame.each(function(x, y, player) {
+      var highestSensorVal;
+      var highestSensorDir;
+      var lowestSensorVal;
+      var lowestSensorDir;
+
+      for (var i = 0; i < player.waveSensors.length; i++) {
+        if (!highestSensorVal || player.waveSensors[i] > highestSensorVal) {
+          highestSensorVal = player.waveSensors[i];
+          highestSensorDir = i;
+        }
+        if (!lowestSensorVal || player.waveSensors[i] > lowestSensorVal) {
+          lowestSensorVal = player.waveSensors[i];
+          lowestSensorDir = i;
+        }
+      }
+
+      player.waveSensors = [0,0,0,0,0,0,0,0];
+      player.missileSensors = [0,0,0,0,0,0,0,0];
+      player.ammo = Math.min(10, player.ammo + 1);
+      playersFrame.write(x, y, player);
+
+      if (highestSensorVal) {
+        if (player.ammo > 0) {
+          var dir = (highestSensorDir + 4) % 8;
+          fireMissile(player, x, y, dir);
+        } else {
+          dir = (lowestSensorDir + 4) % 8;
+          movePlayer(player, x, y, dir);
+        }
+      } else if (Math.random() < 0.1) {
+        movePlayer(player, x, y, Math.floor(Math.random() * 8));
+      }
     });
 
     setTimeout(window.requestAnimationFrame.bind(window, mainLoop), 50);
     //window.requestAnimationFrame(mainLoop);
-  }
-
-  // Player logic
-  function innerLoop(x, y, data, dontDeduce) {
-    var step = data.step % data.pattern.length;
-    var direction = data.pattern[step];
-
-    if (data.inverse) {
-      var targetX = x - movements[data.side][direction][0];
-      var targetY = y - movements[data.side][direction][1];
-    } else {
-      targetX = x + movements[data.side][direction][0];
-      targetY = y + movements[data.side][direction][1];
-    }
-
-    var target = frame.read(targetX, targetY);
-    var destX, destY;
-
-    if (typeof target === 'undefined') {
-      // Cell is empty
-      destX = targetX;
-      destY = targetY;
-    } else if (data.side === target.side) {
-      // Cell is occupied by ally
-      destX = x;
-      destY = y;
-    } else {
-      // Cell is occupied by enemy
-      if (data.amp >= target.amp || data.side === 1 && Math.random() < 0.20) {
-        // Battle won
-        destX = targetX;
-        destY = targetY;
-        data.amp += target.amp;
-        stats[target.side].count--;
-      } else {
-        // Purple can escape blue
-        if (data.side === 2 && target.side === 1 && Math.random() < 0.7) {
-          destX = x;
-          destY = y;
-        } else {
-          // Battle lost
-          target.amp += data.amp;
-          frame.write(targetX, targetY, target);
-          stats[data.side].count--;
-        }
-      }
-    }
-
-    frame.remove(x, y);
-
-    if (typeof destX !== 'undefined') {
-      var energy = energyFrame.read(destX, destY);
-
-      if (energy) {
-        var amount = Math.min(data.side ? 500 : 5000, energy.amp);
-        data.amp += amount;
-        energy.amp -= amount;
-        if (energy.amp > 0) {
-          energyFrame.write(destX, destY, energy);
-        } else {
-          energyFrame.remove(destX, destY);
-        }
-      }
-
-      data.step++;
-
-      if (!dontDeduce) {
-        data.amp -= 1;
-      }
-
-      recycledEnergy++;
-
-      if (data.amp < 1) {
-        // No more energy :(
-        stats[data.side].count--;
-        return;
-      }
-
-      // Clone if possible
-      if (data.amp > data.cloneThreshold * 2 && x !== destX && y !== destY) {
-        if (!frame.read(x, y)) {
-          data.amp -= data.cloneThreshold;
-          clone(frame, data, x, y);
-          stats[data.side].count++;
-        }
-      }
-
-      frame.write(destX, destY, data);
-    }
   }
 
   // Set up user interface and events
